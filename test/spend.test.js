6f50c1eb5904b3bd919955f6e01db4be0486c9cd
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-06-02 12:15:15 -0400

    change to builder style, add OP_RETURN support

--- test/spend.test.js
+++ test/spend.test.js
@@ -1,7 +1,7 @@
 var assert = require('assert')
 // var Blockchain = require('cb-insight')
 var fixtures = require('./fixtures')
-var spend = require('../')
+var Spender = require('../')
 
 /* global describe, it */
 
@@ -9,24 +9,27 @@ describe('spend', function () {
   it('should create and submit Bitcoin testnet transaction', function (done) {
     var f0 = fixtures.valid[0]
     // stub this out
-    spend.blockchain = {
-      addresses: {
-        unspents: function (addresses, callback) {
-          callback(null, f0.utxos)
+    var spender =  new Spender(f0.senderWIF)
+      .to(f0.receiver)
+      .satoshis(f0.amount)
+      .data(new Buffer('big spender'))
+      .blockchain({
+        addresses: {
+          unspents: function (addresses, callback) {
+            callback(null, f0.utxos)
+          }
+        },
+        transactions: {
+          propagate: function (rawTx, callback) {
+            callback()
+          }
         }
-      },
-      transactions: {
-        propagate: function (rawTx, callback) {
-          callback()
-        }
-      }
-    }
-
-    spend(f0.senderWIF, f0.receiver, f0.amount, function (err, txId, rawTx) {
-      assert.ifError(err)
-      assert.equal(rawTx, f0.tx)
-      assert.equal(txId, f0.txId)
-      done()
-    })
+      })
+      .spend(function (err, txId, rawTx) {
+        assert.ifError(err)
+        assert.equal(rawTx, f0.tx)
+        assert.equal(txId, f0.txId)
+        done()
+      })
   })
 })
